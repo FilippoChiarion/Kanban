cc00d5a6a7d87daff41f34d4f6566a7b0063621d
FilippoChiarion <filippochiarion02@gmail.com>
2025-01-16 10:44:52 -0300

    First commit

---
+++ Kanban/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Kanban.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kanban.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AtividadeModel> Atividades { get; set; }
+        public DbSet<StatusModel> Status { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<StatusModel>().HasData(
+                    new StatusModel { Id = 1, Nome = "Pendent"},
+                    new StatusModel { Id = 2, Nome = "Doing"},
+                    new StatusModel { Id = 3, Nome = "Done"}
+                
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Kanban/Dto/CadastroAtividadeDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kanban.Dto
+{
+    public class CadastroAtividadeDto
+    {
+        [Required(ErrorMessage = "Enter the title")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Enter the description")]
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Select a status")]
+        public int StatusId { get; set; }
+    }
+}
